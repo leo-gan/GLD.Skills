e4ec58b135d6ddd3a10e52f81cee5e6e3632cd71
Leonid Ganeline <leo.gan.57@gmail.com>
2015-12-14 13:00:33 -0800

    FullCountingSort finished

--- FullCountingSort/Program.cs
+++ FullCountingSort/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CountingSort2 {
+namespace FullCountingSort {
     internal class Program {
         private static void Main(string[] args) {
             var recordNumber = Convert.ToInt32(Console.ReadLine());
@@ -13,13 +13,13 @@ namespace CountingSort2 {
             for (var i = 0; i < recordNumber; i++) {
                 var recordAsStringArr = Console.ReadLine().Split(' ');
                 recordInt[i] = Convert.ToInt32(recordAsStringArr[0]);
-                recordString[i] = recordAsStringArr[1];
+                recordString[i] = i < recordNumber/2 ? "-" : recordAsStringArr[1];
                 if (recordPositions[recordInt[i]] == null) recordPositions[recordInt[i]] = new List<int>();
                 recordPositions[recordInt[i]].Add(i);
             }
 
             foreach (var recordPosition in recordPositions.Where(recordPositionList => recordPositionList != null).SelectMany(recordPositionList => recordPositionList)) {
-                Console.WriteLine(recordInt[recordPosition] + " " + recordString[recordPosition]);
+                Console.Write( recordString[recordPosition] + " ");
             }
         }
     }
