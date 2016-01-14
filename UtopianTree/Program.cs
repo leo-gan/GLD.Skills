96e4412e66ff3bcad26abe62b3a14fe020169409
Leonid Ganeline <leo.gan.57@gmail.com>
2016-01-14 14:30:25 -0800

    TreeHigh

---
+++ UtopianTree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var tests = Convert.ToInt32(Console.ReadLine());
+            var results = "";
+            for (int test = 0; test < tests; test++) {
+                var cycles = Convert.ToInt32(Console.ReadLine());
+                results += TreeHigh(cycles) + Environment.NewLine;
+            }
+            Console.WriteLine(results);
+        }
+
+        private static string TreeHigh(int cycles) {
+            long high = 1;
+            for (var cycle = 0; cycle < cycles; cycle++)
+                if (cycle%2 == 1)
+                    high += 1;
+                else
+                    high *= 2;
+            return high.ToString();
+        }
+    }
+}
