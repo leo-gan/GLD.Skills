3cb8694ab6ae1a7cdb9aa82130e38b4ee8e94140
leo_g <leo.gan.57@gmail.com>
2018-04-16 14:40:18 -0700

    added selection_sort

---
+++ Sorts/merge_sort.py
@@ -0,0 +1,57 @@
+def merge_sort(x):
+
+    if len(x) < 2: return x
+
+    result, mid = [], int(len(x) / 2)
+
+    y, z = merge_sort(x[:mid]), merge_sort(x[mid:])
+
+    while (len(y) > 0) and (len(z) > 0):
+        t = z if z[0] < y[0] else y
+        print(t[0])
+        result.append(t.pop(0))
+    print(result, y, z)
+    return result + y + z
+
+def mergeSort(ar):
+    print('S:', ar)
+    if len(ar)>1:
+        mid = len(ar) // 2
+        lefthalf = ar[:mid]
+        righthalf = ar[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i, j, k = 0, 0, 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                ar[k]=lefthalf[i]
+                i += 1
+            else:
+                ar[k]=righthalf[j]
+                j += 1
+            k += 1
+
+        while i < len(lefthalf):
+            ar[k]=lefthalf[i]
+            i += 1
+            k += 1
+
+        while j < len(righthalf):
+            ar[k]=righthalf[j]
+            j += 1
+            k += 1
+    print("M:", ar)
+
+
+
+import random
+def main():
+    ar = random.sample(range(1,100),20)
+    print(ar)
+
+    print(merge_sort(ar))
+    print(mergeSort(ar))
+
+main()
