1065eeb71e8885c968dd8be1a4bb07519af0410b
leo_g <leo.gan.57@gmail.com>
2018-04-17 11:20:33 -0700

    quick_srt with interesting output

--- Sorts/quick_sort.py
+++ Sorts/quick_sort.py
@@ -10,19 +10,32 @@ def quick_sort(ar):
     return head + [elem for elem in ar if elem == pivot] + tail
 
 import random
-def qsort(ar):
-    if not ar: return ar
-    pivot = ar[random.choice(range(len(ar)))]  # ar[0] #
+# def qsort(ar):
+#     if not ar: return ar
+#     pivot = ar[random.choice(range(len(ar)))]  # ar[0] #
+#
+#     head = qsort([el for el in ar if el < pivot])
+#     tail = qsort([el for el in ar if el > pivot])
+#     return head + [el for el in ar if el == pivot] + tail
 
-    head = qsort([el for el in ar if el < pivot])
-    tail = qsort([el for el in ar if el > pivot])
-    return head + [el for el in ar if el == pivot] + tail
-
+import random
+def quick_srt(ar):
+    if len(ar) < 2: return ar
+    pivot = ar[0]
+    head = [el for el in ar if el < pivot]
+    middle = [el for el in ar if el == pivot]
+    tail = [el for el in ar if el > pivot]
+    print('   >',  pivot, head, middle, tail)
+    head = quick_srt(head)
+    tail = quick_srt(tail)
+    print('     <', pivot, head, middle, tail)
+    return head + middle + tail
 import random
 def main():
     ar = random.sample(range(10, 100), 20)
     print(ar, '\n')
-    print(quick_sort(ar), '\n')
-    print(qsort(ar))
+    # print(quick_sort(ar), '\n')
+    # print(qsort(ar))
+    print(quick_srt(ar))
 
 main()
