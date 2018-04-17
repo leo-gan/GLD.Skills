0d8befeeab66b191c5cea09cd049ab3ec1c5fdf4
leo_g <leo.gan.57@gmail.com>
2018-04-17 16:11:02 -0700

    added radix_sort

--- Sorts/insertion_sort.py
+++ Sorts/insertion_sort.py
@@ -1,10 +1,16 @@
+# def insertion_sort(ar):
+#     for i in range(len(ar)):
+#         for ii in range(i, 0, -1):
+#             if ar[ii] < ar[ii-1]:
+#                 ar[ii], ar[ii-1] = ar[ii-1], ar[ii]
+#                 print(ar)
+
 def insertion_sort(ar):
     for i in range(len(ar)):
         for ii in range(i, 0, -1):
-            if ar[ii] < ar[ii-1]:
-                ar[ii], ar[ii-1] = ar[ii-1], ar[ii]
-                print(ar)
-
+            if ar[ii-1] > ar[ii]:
+                ar[ii-1], ar[ii] = ar[ii], ar[ii-1]
+                print(ar[ii-1], '<->', ar[ii], ar)
 import random
 def main():
     ar = random.sample(range(1, 100), 20)
---
+++ Sorts/radix_sort.py
@@ -0,0 +1,30 @@
+def list_to_baskets(ar, base, position):
+    baskets = [[] for b in range(base)]
+    for el in ar:
+        digit = (el//(base**position))%base
+        baskets[digit].append(el)
+    print(baskets)
+    return baskets
+
+def baskets_to_list(baskets):
+    return sum(baskets, [])
+
+def radix_sort(ar):
+    base = 10
+    pos = 0 # right side
+    print(base**pos, max(ar))
+    while (base**(pos)) < max(ar):
+        ar = baskets_to_list(list_to_baskets(ar, base, pos))
+        pos += 1
+        print(pos, ar)
+    print(ar)
+    return ar
+
+import random
+def main():
+    ar = random.sample(range(0,1000), 20)
+    print(ar, '\n')
+
+    radix_sort(ar)
+
+main()
